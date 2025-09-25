14ae1c05dee4a3d363d7139beb82c5b3d125dd03
KevinSSantos <kevin_s_santos@estudante.sc.senai.br>
2025-09-25 20:56:22 -0300

    revisado

--- index.js
+++ index.js
@@ -1,45 +1,47 @@
-//Features Add de Metas - Mostrar metas - Menu interativo
-
 const { input, select, checkbox } = require('@inquirer/prompts');
+const fs = require('fs').promises;
 
-console.log("=== Sistema de Metas ===");
+
+async function salvarMetas() {
+    try {
+        await fs.writeFile('metas.json', JSON.stringify(metas, null, 2));
+        mostrarMensagem("✔️  Metas salvas com sucesso!");
+    } catch (error) {
+        mostrarMensagem("❌ Erro ao salvar metas: ",  error.mensagem);
+    }
+}
 
 
 let metas = [];
 
 function limparTela() {
-    console.clear();                    //limpando a tela do console
+  console.clear();
 }
-
-
-
 
 function mostrarMensagem(mensagem) {
     console.log(`\n${mensagem}\n`);
 }
 
-
-
 async function mostrarMenu() {
     const opcao = await select({
-        message: "O que você deseja fazer?",
+        message: "O que você gostaria de fazer?",
         choices: [
             { name: "Adicionar nova meta", value: "adicionar" },
             { name: "Mostrar todas as metas", value: "mostrar" },
-            { name: "Marcar metas como concluídas", value: "marcar" },
+            { name: "Marcar metas como realizadas", value: "marcar" },
             { name: "Mostrar metas realizadas", value: "realizadas" },
-            { name: "Mostrar metas em aberto", value: "aberto"},
-            { name: "Deletar metas", value: "deletar"},
+            { name: "Mostrar metas abertas", value: "abertas" },
+            { name: "Deletar metas", value: "deletar" },
             { name: "Sair", value: "sair" }
         ]
-    })
+    });
 
     return opcao;
 }
 
 async function executarAcao(opcao) {
     switch (opcao) {
-        case "adicionar":
+        case "adicionar": 
             await adicionarMeta();
             break;
         case "mostrar":
@@ -50,63 +52,23 @@ async function executarAcao(opcao) {
             break;
         case "realizadas":
             await metasRealizadas();
-            break;   
-        case "aberto":
-             await metasAberto();
+            break; 
+        case "abertas":
+            await metasAbertas();
             break;
         case "deletar":
             await deletarMetas();
-            break;       
+            break;           
         case "sair":
-            console.log("Saindo do sistema. Até mais!👋");
             break;
         default:
-            console.log("❌ Opção inválida. Tente novamente.");
+            console.log("Opção inválida. Tente novamente. ❌");
     }
-}
-//inicializando a lista de metas vazia
-
-async function iniciar() {
-    while (true) {
-        const opcao = await mostrarMenu();
-
-        if (opcao === "sair") {
-            await executarAcao(opcao);
-            break;
-        }
-
-        await executarAcao(opcao);
-    }
-}
-//CRIANDO UMA FUÇÃO QUE ADICIONA A META ESCRITA
-
-//esse async pq o input tem um tempo de delay 
-//Função assincrona 
-async function adicionarMeta() {
-    const descricao = await input({
-        message: "Digite sua nova Meta: "
-    });
-
-    if (descricao.length === 0) {
-        mostrarMensagem("❌ Meta inválida. Tente novamente.")
-        return;
-    }
-
-    //vira um objeto que inicia com o checked como false
-    const novaMeta = {
-        value: descricao, //q ta ali em cima
-        checked: false
-    }    //tem que mostrar o conteudo de onjeto
-
-    metas.push(novaMeta);  //dps empurra pro banco de dados (let metas [])               //adicionando a nova meta à lista
-
-    console.log("✅ Meta adicionada com sucesso!");
-
 }
 
 async function iniciar() {
     limparTela();
-    mostrarMensagem("Bem-vindo ao sistema de metas pessoais!");
+    mostrarMensagem("=== 📱Sistema de Metas Pessoais ===");
 
     while (true) {
         const opcao = await mostrarMenu();
@@ -114,156 +76,137 @@ async function iniciar() {
         if (opcao === "sair") {
             await executarAcao(opcao);
             limparTela();
-            mostrarMensagem("Até mais!👋");
+            mostrarMensagem("Até mais! 👋🏽");
             break;
         }
 
-        await executarAcao(opcao);   //Para esperar um pouco 
-    }            //chamando a função para adicionar a metawhile 
-    //limpando a tela do console
+        await executarAcao(opcao);
+        await salvarMetas();
+    }
 }
 
+async function adicionarMeta() {
+  const descricao = await input({
+    message: "📝 Digite sua nova meta pessoal:"
+  });
+
+  if (descricao.length === 0) {
+    mostrarMensagem("❌ Meta inválida. Tente novamente.");
+    return;
+  }
+
+  const novaMeta = {
+    value: descricao,
+    checked: false
+  }
+
+  metas.push(novaMeta);
+
+  mostrarMensagem("✅ Meta adicionada com sucesso!");
+}
 
 async function mostrarMetas() {
-    if (metas.length === 0) {               //se a lista de metas estiver vazia
-        console.log("Nenhuma meta cadastrada.");
+    if (metas.length === 0) {
+        mostrarMensagem(" ⛔ Não existem metas cadastradas!");
         return;
     }
-    console.log("Suas metas são:");
-    metas.forEach((meta, index) => {                //percorrendo a lista de metas
-        const status = meta.checked ? "[x]" : "[ ]";   //Verifica se está checado ou não
-        console.log(`${status} ${index + 1}. ${meta.value}`);
+
+    console.log("📚 Suas Metas Pessoais:");
+    metas.forEach((meta, index) => {
+      const status = meta.checked ? "[x]" : "[ ]";  
+      console.log(`${status} ${index + 1}. ${meta.value}`);
     });
 }
 
-//async function main(){  
-//await adicionarMeta();               //chamando a função para adicionar a meta
-//await mostrarMetas();                //chamando a função para mostrar as metas
 
 async function marcarMetas() {
     if (metas.length === 0) {
-        mostrarMensagem("Nenhuma meta cadastrada.");
+        mostrarMensagem(" ⛔ Não existem metas cadastradas!");
         return;
-    }                                                           //VER MAP tem q ver oq é esse trem 
+    }
 
     const metasSelecionadas = await checkbox({
-        message: "Marque as metas que você concluiu:",
-        choices: metas.map(meta =>
-        ({
-            name: meta.value,                                    //dá pra fazer a seleção 
-            value: meta.value,
-            checked: meta.checked
-        }))
-
+        message: "📝 Selecione as metas que você já concluiu:",
+        choices: metas.map(meta => 
+            ({ name: meta.value, 
+               value: meta.value, 
+               checked: meta.checked 
+            })),
     })
 
-    metas.forEach(meta => meta.checked = false); //desmarcando todas as metas
+    metas.forEach(meta => meta.checked = false);
 
     metasSelecionadas.forEach(metaSelecionada => {
         const meta = metas.find(m => m.value === metaSelecionada);
         if (meta) {
-            meta.checked = true; //marcando como concluída
+            meta.checked = true;
         }
     });
 
     mostrarMensagem("✅ Metas atualizadas com sucesso!");
-
 }
 
 async function metasRealizadas() {
     const realizadas = metas.filter(meta => meta.checked);
 
     if (realizadas.length === 0) {
-        mostrarMensagem("Nenhuma meta concluída ainda.");
+        mostrarMensagem("❌ Não existem metas realizadas!");
         return;
     }
-    
-    console.log("Metas concluídas:");
+
+    console.log("✅ Metas Realizadas:");
     realizadas.forEach((meta, index) => {
         console.log(`${index + 1}. ${meta.value}`);
     });
 
+    mostrarMensagem(`Parabéns Você já concluiu ${realizadas.length} metas! 🎉`);
 
- mostrarMensagem(`Parabens você já concluiou ${realizadas.length} metas! 🎉`);
-
+    
 }
 
-//Aberto
+async function metasAbertas() {
+    const abertas = metas.filter(meta => !meta.checked);
 
-async function metasAberto() {
-    const aberto = metas.filter(meta => meta.checked === false);
-
-    if (aberto.length === 0) {
-        mostrarMensagem("Nenhuma meta em aberto.");
+    if (abertas.length === 0) {
+        mostrarMensagem("❌ Não existem metas abertas!");
         return;
     }
-    
-    console.log("Metas em aberto:");
-    aberto.forEach((meta, index) => {
+
+    console.log("📋 Metas Abertas:");
+    abertas.forEach((meta, index) => {
         console.log(`${index + 1}. ${meta.value}`);
     });
 
-
- mostrarMensagem(`Você tem ${aberto.length} meta(s) em aberto!`);
-
+    mostrarMensagem(`Você ainda tem ${abertas.length} metas para concluir. Vamos lá! 🚀`);
 }
 
 async function deletarMetas() {
+    
     if (metas.length === 0) {
-        mostrarMensagem("Não existem metas cadastradas")
+        mostrarMensagem("❌ Não existem metas cadastradas!");
         return;
     }
+
+    const metasParaDeletar = await checkbox({
+        message: "📝 Selecione as metas que deseja deletar:",
+        choices: metas.map(meta => 
+            ({ name: meta.value, 
+               value: meta.value, 
+               checked: false
+            })),
+    });
+
+    if(metasParaDeletar.length === 0) {
+        mostrarMensagem("‼️ Nenhuma meta foi selecionada para deletar")
+        return;
+    }
+
+    metasParaDeletar.forEach(metaParaDeletar => {
+        metas = metas.filter(meta => meta.value !== metaParaDeletar);
+    })
+
+    mostrarMensagem("✅ Metas deletadas com sucesso!");
+
 }
 
-const mestasParaDeletar = await checkbox ({
-    message: "Selecione metas que deseja deletar:",
-    choices: metas.map(meta =>
-    ({name: meta.value,
-     value: meta.value,
-     checked: false
-  })),
-});
-
-if(mestasParaDeletar.length ===0) {
-    mostrarMensagem("Nenhuma meta foi selecionada para deletar")
-    return;
-}
-    mestasParaDeletar.forEach(metaParaDeletar => {
-     metas = metas.filter(meta => meta.value !== metaParaDeletar);
-
-})
-
-mostrarMensagem("Metas deletadas com sucesso!")
-    
-
-
-iniciar();                             //chamando a função principal
-/*console.log("Metas atualizadas:", metas.length);
-adicionarMeta(); //chamando a função para adicionar a meta
-console.log("Metas Finais:", metas);*/
-//main();                                  //chamando a função principal
-
-
-
-/*
-//um objeto
-let meta = { 
-    value: "teste",
-}
-
-let metas = [ //Lista de metas
-    { value: "Passar no curso" },
-    { value: "Ler 2 livros por mês" },
-    { value: "Viajar para o Chile" }
-];
-
-console.log("Meta indívidual:", meta.value);
-console.log("Lista de metas:", metas);
-//simulação do cadastro de uma pessoa
-let pessoa = {
-    nome: "João",
-    idade: 30,
-    metas: metas //associando a lista de metas à pessoa
-};
-
-console.log("Dados da pessoa cadastrada (idade):", pessoa.idade);*/
+iniciar();
